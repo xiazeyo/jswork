731264de95163b0a4abd9738808baf7b54a05495
xiazeyo <2595655035@qq.com>
2019-10-15 21:18:34 +0800

    work2

--- work4/work4.js
+++ work4/work4.js
@@ -4,26 +4,28 @@ level = parseFloat(level)&&Number(level)
 if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
-for(let i=1;i<=level;++1){
+for (let i=1;i<=level;++i){
     let blank = level - i;
     for(let k=0;k<blank;++k){
-        ducument.write('&nbsp;');
+        document.write('&nbsp;');
     }
     //打印星星
-    let star = i * 2 - 1
-    for(let j=0;j<star;++j){
+    let star = i * 2 -1
+    for(let j=0;j<star;++j)(
         document.write('*')
-    }
-
-    let str = '<table border="1">'
-    for(let i=1;i<10;++j){
+    )
+    //换行
+    document.write("<br>")
+}
+let str = '<table border="1">'
+for(let i=1;i<10;++i){
+    str += '<tr>'
+    for(var j=1;j<=i;++j){
         //; 拼接单元格
-        str += '<td>' + j + '*' + i + '=' + ( j * j ) + '</td>'
-    }
+        str += '<td>' + j + '=' + (j * i) + '</td>'    
+    }    
     str += '</tr>'
 }
 str += '</table>'
 //设置div的html文档内容
-document.getElementById('table').innerHTML = str
-
-}
+document.getElementById('table').innerHTML = str    

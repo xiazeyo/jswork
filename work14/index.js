ba76c3809cdb27dca43b427ae2ecb33db62365a3
xiazeyo <2595655035@qq.com>
2019-12-08 12:43:03 +0800

    work14

--- work14/index.js
+++ work14/index.js
@@ -1,7 +1,8 @@
-let endseconds = new Date().getTime() + 15 + 1000;
+let endseconds = new Date().getTime() + 15 * 1000;
 let d = h = m = s = 0;
 let id = setInterval(seckill, 1000);
-function seckill {
+
+function seckill() {
     let nowtime = new Date();
     let remaining = parseInt((endseconds - nowtime.getTime()) / 1000);
     if (remaining > 0) {
@@ -9,14 +10,17 @@ function seckill {
         h = parseInt((remaining / 3600) % 24);
         m = parseInt((remaining / 60) % 60);
         s = parseInt(remaining % 60);
+        d = d < 10 ? '0' + d : d;
+        h = h < 10 ? '0' + h : h;
+        m = m < 10 ? '0' + m : m;
+        s = s < 10 ? '0' + s : s;
     } else {
         clearInterval(id);
-        d = h = m = s = '00'
+        d = h = m = s = '00';
     }
     document.getElementById('d').innerHTML = d + '天';
-    document.getElementById('h').innerHTML = d + '时';
-    document.getElementById('m').innerHTML = d + '分';
-    document.getElementById('s').innerHTML = d + '秒';
-
+    document.getElementById('h').innerHTML = h + '时';
+    document.getElementById('m').innerHTML = m + '分';
+    document.getElementById('s').innerHTML = s + '秒';
 }
     
